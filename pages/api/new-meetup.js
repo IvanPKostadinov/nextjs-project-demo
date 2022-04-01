31281e7691f51991bf0a922cc817595f549e0216
IvanPKostadinov <ikostadinov@melontech.com>
2022-04-01 18:15:16 +0300

    created a basic API Route to add data to a MongoDB database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,34 @@
+import { MongoClient } from 'mongodb';
+
 // domain.com/api/new-meetup
 
-function handler(req, res) {
+const USER_NAME = 'ivanpkostadinov';
+const USER_PASSWORD = 'GCRJ2agW5xYdpPeh';
+const DATABASE_NAME = 'meetups';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     // triggered in: POST to domain.com/api/new-meetup
     const data = req.data;
-    
-    const { title, image, address, description } = data;
+
+    // const { title, image, address, description } = data;
+
+    // Connection string from cloud.mongodb.com -> the Cluster we want to connect to:
+    // This will give us a connected client:
+    const client = await MongoClient.connect(
+      `mongodb+srv://${USER_NAME}:${USER_PASSWORD}@cluster0.mhrjm.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups'); // may have different name from DATABASE_NAME
+
+    const result = await meetupsCollection.insertOne(data); // { title: ..., image: ..., ...}
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted.' });
   }
 }
 
