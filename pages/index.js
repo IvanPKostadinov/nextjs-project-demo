d812f5aa0b9f8cafc983889ecd712bef10cfeec0
IvanPKostadinov <ikostadinov@melontech.com>
2022-04-01 15:24:32 +0300

    added useEffect() in index.js to simulate fetching data from a server

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -20,7 +22,14 @@ const DUMMY_MEETUPS = [
 ];
 
 function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    // send http request and fetch data
+    setLoadedMeetups(DUMMY_MEETUPS);
+  }, []);
+
+  return <MeetupList meetups={loadedMeetups} />;
 }
 
 export default HomePage;
