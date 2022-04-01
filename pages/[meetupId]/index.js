06577a500d926f43bc8a40986fbe8e49aeb9da07
IvanPKostadinov <ikostadinov@melontech.com>
2022-04-01 16:35:08 +0300

    added getStaticProps() to [meetupId] and used context to get the concrete Meetup id from the URL

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -2,15 +2,35 @@ import { Fragment } from 'react';
 
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1280px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG'
-      title='A first meetup'
-      address='Some street 5'
-      description='The meetup description.'
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+  const meetupId = context.params.meetupId; // contains the dynamic URL part
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1280px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG',
+        id: meetupId,
+        title: 'A first meetup',
+        address: 'Some street 5',
+        description: 'The meetup description.',
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
