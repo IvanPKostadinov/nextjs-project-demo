f0a622a9c3177563eb6b41172312203d4fa7292e
IvanPKostadinov <ikostadinov@melontech.com>
2022-04-01 15:39:46 +0300

    added async function getStaticProps() to fetch the data we need before the first rendering of the component(during Build-up) to show it in the Source Code (in the Browser)

--- pages/index.js
+++ pages/index.js
@@ -21,15 +21,28 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  const [loadedMeetups, setLoadedMeetups] = useState([]);
+function HomePage(props) {
+  // const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  useEffect(() => {
-    // send http request and fetch data
-    setLoadedMeetups(DUMMY_MEETUPS);
-  }, []);
+  // useEffect(() => {
+  //   // send http request and fetch data
+  //   setLoadedMeetups(DUMMY_MEETUPS);
+  // }, []);
 
-  return <MeetupList meetups={loadedMeetups} />;
+  return <MeetupList meetups={props.meetups} />;
+}
+
+// If we fetch data after rendering the Component that we want to show in the Source Code in the Browser:
+// This function is called BEFORE the Component function!
+// If async -> Next.js will wait for this data to be loaded, before it executes the Comp. function!
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    // these props will be set as props of the Page Component:
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 }
 
 export default HomePage;
