bf98d0c4456241dadb3a7598f510900b83b3c989
IvanPKostadinov <ikostadinov@melontech.com>
2022-04-04 14:25:44 +0300

    fetched the data for the Details page from MongoDB

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+import { MongoClient, ObjectId } from 'mongodb';
 
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
@@ -13,21 +13,39 @@ function MeetupDetails(props) {
   );
 }
 
-export function getStaticPaths() {
+export async function getStaticPaths() {
+  const USER_NAME = 'ivanpkostadinov';
+  const USER_PASSWORD = 'FsFvDlScnESbAXI9';
+  const DATABASE_NAME = 'meetups';
+
+  const client = await MongoClient.connect(
+    `mongodb+srv://${USER_NAME}:${USER_PASSWORD}@cluster0.mhrjm.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups'); // may have different name from DATABASE_NAME
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: 'm1',
-        },
-      },
-      {
-        params: {
-          meetupId: 'm2',
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+    // [
+    //   {
+    //     params: {
+    //       meetupId: 'm1',
+    //     },
+    //   },
+    //   {
+    //     params: {
+    //       meetupId: 'm2',
+    //     },
+    //   },
+    // ],
   };
 }
 
@@ -38,16 +56,38 @@ export async function getStaticProps(context) {
   // this will be only seen in the terminal(on the Developer Server-side), not in the Browser
   console.log(meetupId);
 
+  const USER_NAME = 'ivanpkostadinov';
+  const USER_PASSWORD = 'FsFvDlScnESbAXI9';
+  const DATABASE_NAME = 'meetups';
+
+  const client = await MongoClient.connect(
+    `mongodb+srv://${USER_NAME}:${USER_PASSWORD}@cluster0.mhrjm.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups'); // may have different name from DATABASE_NAME
+
+  const selectedMeetup = await meetupsCollection.findOne({ _id: ObjectId(meetupId) });
+
+  client.close();
+
   return {
     props: {
       meetupData: {
-        image:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1280px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG',
-        id: meetupId,
-        title: 'A first meetup',
-        address: 'Some street 5',
-        description: 'The meetup description.',
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        image: selectedMeetup.image,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
       },
+      // meetupData: {
+      //   image:
+      //     'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1280px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG',
+      //   id: meetupId,
+      //   title: 'A first meetup',
+      //   address: 'Some street 5',
+      //   description: 'The meetup description.',
+      // },
     },
   };
 }
